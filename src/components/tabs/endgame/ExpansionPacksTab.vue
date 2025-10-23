cd44f21aa94b87deea1db0c3ff99f0460b30197e
Supersonic Seven <b.rockholz@icloud.com>
2025-10-23 10:00:42 -0400

    Update ExpansionPacksTab.vue
    That should look better

--- src/components/tabs/endgame/ExpansionPacksTab.vue
+++ src/components/tabs/endgame/ExpansionPacksTab.vue
@@ -54,39 +54,53 @@ export default {
 
 <style scoped>
 .c-primary-btn--expansion-packs-unlock {
-  width: 20rem;
-  height: 8rem;
-  font-family: Typewriter, serif;
-  font-size: 1rem;
+  display: flex;
+  flex-direction: column;
+  width: 18rem;
+  height: 9rem;
+  position: relative;
+  justify-content: center;
+  align-items: center;
+  font-family: Typewriter;
+  font-size: 0.95rem;
   font-weight: bold;
-  border: 0.1rem solid linear-gradient(var(--color-endgame), var(--color-pelle--base));
+  color: var(--color-text);
+  background: var(--color-text-inverted);
+  border: 0.1rem solid var(--color-endgame);
   border-radius: var(--var-border-radius, 0.4rem);
-  transition-duration: 0.2s;
+  box-shadow: inset 0 0 1rem 0.1rem var(--color-endgame);
+  margin: 0.6rem 0.55rem;
+  padding: 1rem;
   cursor: pointer;
+}
+
+.c-primary-btn--expansion-packs-unlock:hover {
+  box-shadow: inset 0 0 2rem 0.1rem var(--color-pelle--base);
+  transition-duration: 0.3s;
 }
 
 .c-primary-btn--expansion-packs-unlock--unavailable {
   color: #181818;
-  background-color: #5f5f5f;
+  background: #5f5f5f;
 }
 
 .c-primary-btn--expansion-packs-unlock--unavailable:hover {
-  background-color: #737373;
+  background: #737373;
 }
 
 .c-primary-btn--expansion-packs-unlock--available {
-  color: linear-gradient(var(--color-endgame), var(--color-pelle--base));
-  background-color: black;
+  color: var(--color-endgame);
+  background: black;
 }
 
 .c-primary-btn--expansion-packs-unlock--available:hover {
   color: var(--color-text);
-  background-color: linear-gradient(var(--color-endgame), var(--color-pelle--base));
+  background: linear-gradient(var(--color-endgame), var(--color-pelle--base));
 }
 
 .c-primary-btn--expansion-packs-unlock--bought {
   color: black;
-  background-color: linear-gradient(var(--color-endgame), var(--color-pelle--base));
-  border-color: black;
+  background: linear-gradient(var(--color-endgame), var(--color-pelle--base));
+  border: 0.1rem solid black;
 }
 </style>
