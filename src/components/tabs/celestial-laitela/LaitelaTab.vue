bfe22614fccc9fdb8786652470dc54ed3bca57ec
Supersonic Seven <b.rockholz@icloud.com>
2025-11-12 14:02:06 -0500

    Update LaitelaTab.vue
    We iterate

--- src/components/tabs/celestial-laitela/LaitelaTab.vue
+++ src/components/tabs/celestial-laitela/LaitelaTab.vue
@@ -27,13 +27,13 @@ export default {
       darkMatterGain: new Decimal(0),
       isDMCapped: false,
       maxDarkMatter: new Decimal(0),
-      darkEnergy: 0,
-      matterExtraPurchasePercentage: 0,
+      darkEnergy: new Decimal(0),
+      matterExtraPurchasePercentage: new Decimal(0),
       autobuyersUnlocked: false,
       singularityPanelVisible: false,
       singularitiesUnlocked: false,
-      singularityCap: 0,
-      singularityWaitTime: 0,
+      singularityCap: new Decimal(0),
+      singularityWaitTime: "",
       showAnnihilation: false
     };
   },
@@ -50,17 +50,17 @@ export default {
       this.darkMatter.copyFrom(Currency.darkMatter);
       this.isDMCapped = this.darkMatter.eq(Laitela.darkMatterCap);
       this.maxDarkMatter.copyFrom(Currency.darkMatter.max);
-      this.darkEnergy = player.celestials.laitela.darkEnergy;
-      this.matterExtraPurchasePercentage = Laitela.matterExtraPurchaseFactor - 1;
+      this.darkEnergy.copyFrom(player.celestials.laitela.darkEnergy);
+      this.matterExtraPurchasePercentage.copyFrom(Laitela.matterExtraPurchaseFactor.sub(1));
       this.autobuyersUnlocked = SingularityMilestone.darkDimensionAutobuyers.canBeApplied ||
         SingularityMilestone.darkDimensionAutobuyers.canBeApplied ||
         SingularityMilestone.autoCondense.canBeApplied ||
         Laitela.darkMatterMult > 1;
       this.singularityPanelVisible = Currency.singularities.gt(0);
       this.singularitiesUnlocked = Singularity.capIsReached || this.singularityPanelVisible;
-      this.singularityCap = Singularity.cap;
-      this.singularityWaitTime = TimeSpan.fromSeconds(new Decimal((this.singularityCap - this.darkEnergy) /
-        Currency.darkEnergy.productionPerSecond)).toStringShort();
+      this.singularityCap.copyFrom(Singularity.cap);
+      this.singularityWaitTime = TimeSpan.fromSeconds(new Decimal((this.singularityCap.sub(this.darkEnergy)).div(
+        Currency.darkEnergy.productionPerSecond))).toStringShort();
       this.showAnnihilation = Laitela.annihilationUnlocked;
 
       const d1 = DarkMatterDimension(1);
