0a66dfbf847c7678af48f8a71e6741c04029b2cc
Supersonic Seven <b.rockholz@icloud.com>
2025-11-22 17:23:04 -0500

    Create GalacticPowerRow.vue
    We can attempt this

---
+++ src/components/tabs/galactic-power/GalacticPowerRow.vue
@@ -0,0 +1,46 @@
+<script>
+import DescriptionDisplay from "@/components/DescriptionDisplay";
+import EffectDisplay from "@/components/EffectDisplay";
+
+export default {
+  name: "GalacticPowerRow",
+  components: {
+    DescriptionDisplay,
+    EffectDisplay
+  },
+  data() {
+    return {
+      isUnlocked: false
+    };
+  },
+  computed: {
+    config() {
+      return GalacticPowers.config;
+    },
+    title() {
+      return `Galactic Power ${this.config.id}`;
+    }
+  },
+  methods: {
+    update() {
+      this.isUnlocked = this.config.isUnlocked;
+    }
+  }
+};
+</script>
+
+<template>
+  <div v-show="isUnlocked">
+    <DescriptionDisplay
+      :config="config.reward"
+      title="title"
+    />
+    <EffectDisplay
+      :config="config.reward"
+    />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
