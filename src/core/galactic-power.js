7157e8875fd40e323d8435f06134c41f468e09c9
Supersonic Seven <b.rockholz@icloud.com>
2025-11-22 15:02:40 -0500

    Update galactic-power.js
    Overhaul

--- src/core/galactic-power.js
+++ src/core/galactic-power.js
@@ -11,9 +11,24 @@ export class GalacticPowerState {
     return Currency.galacticPower.gte(this.unlockGP);
   }
 }
-export const GalacticPower = mapGameDataToObject(
+
+export const GalacticPowers = mapGameDataToObject(
   GameDatabase.endgame.galacticPowers,
   config => (config.isBaseResource
     ? new GalacticPowerState(config)
     : new GalacticPowerState(config))
 );
+
+export const GalacticPower = {
+  get isUnlocked() {
+    return SingularityMilestone.galacticPower.isUnlocked;
+  }
+};
+
+export function getGalacticPowerGainPerSecond() {
+  const galaxyFactor = Math.max(player.galaxies / 100000, 1);
+  const celMatterFactor = Math.max(Math.pow(Decimal.log10(player.endgame.celestialMatter) / 10, 2), 1);
+  const imaginaryFactor = Math.max(math.pow(Decimal.log10(player.reality.imaginaryMachines), 1.5), 1);
+  const galaxyExponent = 1 + Math.pow(player.galaxies / 10000000, 3);
+  return Decimal.pow(galaxyFactor * celMatterFactor * imaginaryFactor, galaxyExponent);
+}
