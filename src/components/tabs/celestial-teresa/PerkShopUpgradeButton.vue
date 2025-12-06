1e96234f5ccb807c593531d51e49b332f6727be5
Supersonic Seven <b.rockholz@icloud.com>
2025-12-06 10:05:45 -0500

    Update PerkShopUpgradeButton.vue
    Finishing Charges

--- src/components/tabs/celestial-teresa/PerkShopUpgradeButton.vue
+++ src/components/tabs/celestial-teresa/PerkShopUpgradeButton.vue
@@ -34,7 +34,7 @@ export default {
         "o-teresa-shop-button": true,
         "o-teresa-shop-button--available": ((this.isAvailableForPurchase && !this.isCapped) ||
           (this.chargesLeft > 0 && !this.isCharged)) && !(this.viewingCharge && this.chargesLeft <= 0),
-        "o-teresa-shop-button--capped": this.isCapped || this.isCharged,
+        "o-teresa-shop-button--capped": (this.isCapped && !this.viewingCharge) || this.isCharged,
         "o-teresa-shop-button--pelle-disabled": this.isDoomed &&
           (this.upgrade === PerkShopUpgrade.musicGlyph || this.upgrade === PerkShopUpgrade.fillMusicGlyph)
       };
