01cb7ccb2bf85a0f28cc96c5e7359134b34d3316
Supersonic Seven <b.rockholz@icloud.com>
2025-12-20 16:09:44 -0500

    Update dark-matter-dimension.js
    Attempting to fix double-click

--- src/core/celestials/laitela/dark-matter-dimension.js
+++ src/core/celestials/laitela/dark-matter-dimension.js
@@ -197,7 +197,7 @@ export class DarkMatterDimensionState extends DimensionState {
     const cost = this.rawIntervalCost.times(
       Decimal.pow(this.intervalCostIncrease, x).minus(1)).div(this.intervalCostIncrease.sub(1)).floor();
     if (!Currency.darkMatter.purchase(cost)) return false;
-    if (Currency.darkMatter.purchase(cost)) {
+    else {
       this.data.intervalUpgrades = this.data.intervalUpgrades.add(x);
       return true;
     }
@@ -207,7 +207,7 @@ export class DarkMatterDimensionState extends DimensionState {
     const cost = this.rawPowerDMCost.times(
       Decimal.pow(this.powerDMCostIncrease, x).minus(1)).div(this.powerDMCostIncrease.sub(1)).floor();
     if (!Currency.darkMatter.purchase(cost)) return false;
-    if (Currency.darkMatter.purchase(cost)) {
+    else {
       this.data.powerDMUpgrades = this.data.powerDMUpgrades.add(x);
       return true;
     }
@@ -217,7 +217,7 @@ export class DarkMatterDimensionState extends DimensionState {
     const cost = this.rawPowerDECost.times(
       Decimal.pow(this.powerDECostIncrease, x).minus(1)).div(this.powerDECostIncrease.sub(1)).floor();
     if (!Currency.darkMatter.purchase(cost)) return false;
-    if (Currency.darkMatter.purchase(cost)) {
+    else {
       this.data.powerDEUpgrades = this.data.powerDEUpgrades.add(x);
       return true;
     }
