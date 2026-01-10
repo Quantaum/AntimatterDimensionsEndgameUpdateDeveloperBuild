bce447e84a11c63e493387fd2f66bd488e02baae
Supersonic Seven <b.rockholz@icloud.com>
2026-01-10 13:23:35 -0500

    Update endgame.js
    3/3

--- src/core/endgame.js
+++ src/core/endgame.js
@@ -1,4 +1,4 @@
-import * as ADNotations from "@antimatter-dimensions/notations";
+import * as ADNotations from "adnot-beport-small";
 
 import { AutomatorPanels } from "@/components/tabs/automator/AutomatorDocs";
 import { GlyphInfo } from "@/components/modals/options/SelectGlyphInfoDropdown";
