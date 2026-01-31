81742c63a3d1378166fa97f858289f331654f057
Supersonic Seven <b.rockholz@icloud.com>
2026-01-30 22:14:01 -0500

    Update antimatter-dimension.js
    Ricoche

--- src/core/dimensions/antimatter-dimension.js
+++ src/core/dimensions/antimatter-dimension.js
@@ -616,7 +616,7 @@ class AntimatterDimensionState extends DimensionState {
         const log10 = production.log10();
         const eg = Currency.endgames.value;
         const endgameMult = Pelle.isDoomed ? 1 + (Math.log10(Math.min(eg, 1e6) * Math.max(Math.log2(eg + 1) - Math.log2(5e5), 1) + 1) / 80) : 1 + (Math.log10(Math.min(eg, 1e6) * Math.max(Math.log2(eg + 1) - Math.log2(5e5), 1) + 1) / 200);
-        const endgameMultValue = EndgameMilestone.endgameAntimatter.isReached ? endgameMult : 1;
+        const endgameMultValue = (EndgameMilestone.endgameAntimatter.isReached && !player.disablePostReality) ? endgameMult : 1;
         production = Decimal.pow10(Decimal.pow(log10, getAdjustedGlyphEffect("effarigantimatter") * Effects.product(EndgameMastery(101), EndgameUpgrade(15), SingularityMilestone.antimatterExponentPower) * endgameMultValue));
       }
       if (production.gt(Decimal.pow10(1e150))) {
