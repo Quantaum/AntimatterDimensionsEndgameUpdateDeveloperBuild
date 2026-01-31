6ae1fbec2c40fccb53524a8660a6f25921b789da
Supersonic Seven <b.rockholz@icloud.com>
2026-01-30 19:23:55 -0500

    Update EnslavedTab.vue
    Temp

--- src/components/tabs/celestial-enslaved/EnslavedTab.vue
+++ src/components/tabs/celestial-enslaved/EnslavedTab.vue
@@ -137,7 +137,7 @@ export default {
       this.offlineEnabled = player.options.offlineProgress;
       this.hasAutoRelease = Ra.unlocks.autoPulseTime.canBeApplied;
       this.isRunning = Enslaved.isRunning;
-      this.completed = Enslaved.isCompleted && !this.isDoomed;
+      this.completed = Enslaved.isCompleted && !this.isDoomed && !player.disablePostReality;
       this.storedReal = player.celestials.enslaved.storedReal;
       this.storedRealEffiency = Enslaved.storedRealTimeEfficiency;
       this.storedRealCap = Enslaved.storedRealTimeCap;
