1d307348c0c6d3f4bd59f5b7bd0f960bce9d3ce8
Supersonic Seven <b.rockholz@icloud.com>
2026-05-05 20:45:16 -0400

    Enhance LargeHadronColliderTab with speed tracking
    Added hadronSpeed data property and computed property for displaying speed text.

--- src/components/tabs/large-hadron-collider/LargeHadronColliderTab.vue
+++ src/components/tabs/large-hadron-collider/LargeHadronColliderTab.vue
@@ -8,12 +8,20 @@ export default {
   },
   data() {
     return {
-      hasAccelerator: false
+      hasAccelerator: false,
+      hadronSpeed: 0
     };
+  },
+  computed: {
+    hadronSpeedText() {
+      if (this.hadronSpeed === 0) return `Your Hadrons are stationary`;
+      return `Your Hadrons are moving at ${format(this.hadronSpeed, 2, 2)} m/s`;
+    },
   },
   methods: {
     update() {
       this.hasAccelerator = Accelerators.all.some(a => a.isUnlocked);
+      this.hadronSpeed = LHC.hadronSpeed;
     },
   }
 };
@@ -22,8 +30,17 @@ export default {
 <template>
   <div class="l-large-hadron-collider-tab">
     <div class="l-large-hadron-collider-all-content-container">
+      <div
+        v-if="hasAccelerator"
+        class="c-large-hadron-collider-description"
+      >
+        {{ hadronSpeedText }}
+      </div>
       <AcceleratorsPanel v-if="hasAccelerator" />
-      <div v-if="!hasAccelerator">
+      <div
+        v-if="!hasAccelerator"
+        class="c-large-hadron-collider-description"
+      >
         Reach {{ format(Decimal.pow10(1e200), 2, 2) }} Antimatter
       </div>
     </div>
@@ -42,5 +59,12 @@ export default {
   flex-direction: column;
   width: 100%;
   align-items: center;
+}
+
+.c-large-hadron-collider-description {
+  position: relative;
+  font-size: 2rem;
+  font-weight: bold;
+  color: var(--color-alpha--base);
 }
 </style>
