063abeba986a5aade4992e12a7c2171b40e5ab72
Supersonic Seven <b.rockholz@icloud.com>
2026-05-21 10:14:21 -0400

    Trigger PelleStrikes decay if QoL milestone is reached

--- src/core/celestials/pelle/pelle.js
+++ src/core/celestials/pelle/pelle.js
@@ -89,6 +89,7 @@ export const Pelle = {
     player.IPMultPurchases = DC.D0;
     Autobuyer.bigCrunch.mode = AUTO_CRUNCH_MODE.AMOUNT;
     if (!DivinityMilestone.pelleQoL.isReached) disChargeAll();
+    if (DivinityMilestone.pelleQoL.isReached) PelleStrikes.decay.trigger();
     if (!Effarig.isRunning || Effarig.currentStage !== EFFARIG_STAGES.ENDGAME) clearCelestialRuns();
     CelestialDimensions.resetAmount();
     player.records.thisEndgame.peakGameSpeed = DC.D1;
