68f397c4777d7e5ac935f3b8afa2ad919adc0ba8
Supersonic Seven <b.rockholz@icloud.com>
2026-06-09 19:57:43 -0400

    Add diff parameter to freeStarReset function
    Modified freeStarReset function to include a diff parameter for scaling reset formula.

--- src/core/ethereal.js
+++ src/core/ethereal.js
@@ -125,13 +125,13 @@ export function resetForStar(id) {
   player.endgame.ethereal.stars[starName] = player.endgame.ethereal.stars[starName].add(resetFormula);
 }
 
-export function freeStarReset(id) {
+export function freeStarReset(id, diff) {
   const gainedStarType = EtherealStars.all.find(x => x.id === id);
   const starName = gainedStarType.config.name;
   const resetReq = gainedStarType.config.resetReq;
   if (Currency.etherealPower.lt(resetReq) || !gainedStarType.isUnlocked) return;
   const resetFormula = Decimal.pow(Currency.etherealPower.value.div(resetReq), 0.5 - id / 20).times(Ethereal.allStarBoost).times(
-    Ethereal.starGeneration(id));
+    Ethereal.starGeneration(id).times(diff).div(1000));
   player.endgame.ethereal.stars[starName] = player.endgame.ethereal.stars[starName].add(resetFormula);
 }
 
