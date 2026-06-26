24de117561398865588e99b1ea5e0e674b96f490
Supersonic Seven <b.rockholz@icloud.com>
2026-06-25 20:34:33 -0400

    Fix infinity dimension variables and descriptions

--- src/components/tabs/infinity-dimensions/ModernInfinityDimensionsTab.vue
+++ src/components/tabs/infinity-dimensions/ModernInfinityDimensionsTab.vue
@@ -34,9 +34,9 @@ export default {
       isEndgameUnlocked: false,
       infinityDimCompressionMagnitude: 0,
       infinityDimOverflow: 0,
+      infinityDimStart: new Decimal(0),
       infinityDimCompressionMagnitude2: 0,
       infinityDimOverflow2: 0,
-      infinityDimStart: new Decimal(0),
       infinityDimStart2: new Decimal(0),
       hasSecond: false,
       freeTesseractSoftcap: 0,
@@ -91,7 +91,7 @@ export default {
       this.infinityDimOverflow = 1 / this.infinityDimCompressionMagnitude;
       this.infinityDimStart = InfinityDimensions.OVERFLOW;
       this.infinityDimCompressionMagnitude2 = InfinityDimensions.compressionMag2;
-      this.infinityDimOverflow2 = 1 / this.infinityDimCompressionMag2;
+      this.infinityDimOverflow2 = 1 / this.infinityDimCompressionMagnitude2;
       this.infinityDimStart2 = InfinityDimensions.OVERFLOW_SQUARED;
       this.hasSecond = Currency.infinityPower.gte(DC.ENUMMAX);
       this.freeTesseractSoftcap = Tesseracts.freeSoftcapStart;
@@ -159,24 +159,24 @@ export default {
     <div>
       <p>
         <span v-if="isEndgameUnlocked">
-          Your Infinity Dimension Compression^2 Magnitude is
-          <span class="c-infinity-dim-compression-description__accent">{{ format(infinityDimCompressionMagnitude2, 2, 3) }}</span>,
-          which raises all Infinity Dimension Multipliers to the power of
-          <span class="c-infinity-dim-compression-description__accent">{{ format(infinityDimOverflow2, 2, 3) }}</span>
-          while above
-          <span>{{ formatPostBreak(infinityDimStart2, 2, 1) }}</span>.
-        </span>
-      </p>
-    </div>
-    <div>
-      <p>
-        <span v-if="hasSecond">
           Your Infinity Dimension Compression Magnitude is
           <span class="c-infinity-dim-compression-description__accent">{{ format(infinityDimCompressionMagnitude, 2, 3) }}</span>,
           which raises all Infinity Dimension Multipliers to the power of
           <span class="c-infinity-dim-compression-description__accent">{{ format(infinityDimOverflow, 2, 3) }}</span>
           while above
           <span>{{ formatPostBreak(infinityDimStart, 2, 1) }}</span>.
+        </span>
+      </p>
+    </div>
+    <div>
+      <p>
+        <span v-if="hasSecond">
+          Your Infinity Dimension Compression^2 Magnitude is
+          <span class="c-infinity-dim-compression-description__accent">{{ format(infinityDimCompressionMagnitude2, 2, 3) }}</span>,
+          which raises all Infinity Dimension Multipliers to the power of
+          <span class="c-infinity-dim-compression-description__accent">{{ format(infinityDimOverflow2, 2, 3) }}</span>
+          while above
+          <span>{{ formatPostBreak(infinityDimStart2, 2, 1) }}</span>.
         </span>
       </p>
     </div>
