dca08d208c73564690a392ec240b5c6d62419107
Supersonic Seven <b.rockholz@icloud.com>
2026-07-04 17:34:33 -0400

    Add Hadrons component to BackgroundAnimations

--- src/components/BackgroundAnimations.vue
+++ src/components/BackgroundAnimations.vue
@@ -1,22 +1,27 @@
 <script>
 import BlobSnowflakes from "@/components/BlobSnowflakes";
+import Hadrons from "@/components/tabs/large-hadron-collider/Hadrons";
 import TachyonParticles from "@/components/tabs/time-dilation/TachyonParticles";
 
 export default {
   name: "BackgroundAnimations",
   components: {
     BlobSnowflakes,
+    Hadrons,
     TachyonParticles
   },
   data() {
     return {
       blob: false,
+      animateHadrons: false,
       animateTachyons: false
     };
   },
   methods: {
     update() {
       this.blob = Theme.currentName() === "S11";
+      this.animateHadrons = player.options.animations.hadrons &&
+        Tabs.current[this.$viewModel.subtab].name === "Large Hadron Collider";
       this.animateTachyons = player.options.animations.tachyonParticles &&
         Tabs.current[this.$viewModel.subtab].name === "Time Dilation";
     }
@@ -30,6 +35,7 @@ export default {
     class="l-background-animations"
   >
     <BlobSnowflakes v-if="blob" />
+    <Hadrons v-if="animateHadrons" />
     <TachyonParticles v-if="animateTachyons" />
   </div>
 </template>
