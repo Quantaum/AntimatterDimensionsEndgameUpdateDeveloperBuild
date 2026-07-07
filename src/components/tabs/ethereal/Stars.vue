87fac4a41fedf8bc97a9cffef9b0da3b1aeba994
Supersonic Seven <b.rockholz@icloud.com>
2026-07-07 12:55:50 -0400

    Limit star counts to 100 and update keys
    Updated star counts to be capped at 100 and modified keys for rendering.

--- src/components/tabs/ethereal/Stars.vue
+++ src/components/tabs/ethereal/Stars.vue
@@ -35,15 +35,15 @@ export default {
   },
   methods: {
     update() {
-      this.rcount = Decimal.floor(player.endgame.ethereal.stars.red.add(1).log10().sqrt().add(0.5)).toNumber();
-      this.ocount = Decimal.floor(player.endgame.ethereal.stars.orange.add(1).log10().sqrt().add(0.5)).toNumber();
-      this.ycount = Decimal.floor(player.endgame.ethereal.stars.yellow.add(1).log10().sqrt().add(0.5)).toNumber();
-      this.gcount = Decimal.floor(player.endgame.ethereal.stars.green.add(1).log10().sqrt().add(0.5)).toNumber();
-      this.bcount = Decimal.floor(player.endgame.ethereal.stars.blue.add(1).log10().sqrt().add(0.5)).toNumber();
-      this.pcount = Decimal.floor(player.endgame.ethereal.stars.purple.add(1).log10().sqrt().add(0.5)).toNumber();
-      this.wcount = Decimal.floor(player.endgame.ethereal.stars.white.add(1).log10().sqrt().add(0.5)).toNumber();
-      this.lcount = Decimal.floor(player.endgame.ethereal.stars.black.add(1).log10().sqrt().add(0.5)).toNumber();
-      this.acount = Decimal.floor(player.endgame.ethereal.stars.gray.add(1).log10().sqrt().add(0.5)).toNumber();
+      this.rcount = Decimal.floor(player.endgame.ethereal.stars.red.add(1).log10().sqrt().add(0.5)).min(100).toNumber();
+      this.ocount = Decimal.floor(player.endgame.ethereal.stars.orange.add(1).log10().sqrt().add(0.5)).min(100).toNumber();
+      this.ycount = Decimal.floor(player.endgame.ethereal.stars.yellow.add(1).log10().sqrt().add(0.5)).min(100).toNumber();
+      this.gcount = Decimal.floor(player.endgame.ethereal.stars.green.add(1).log10().sqrt().add(0.5)).min(100).toNumber();
+      this.bcount = Decimal.floor(player.endgame.ethereal.stars.blue.add(1).log10().sqrt().add(0.5)).min(100).toNumber();
+      this.pcount = Decimal.floor(player.endgame.ethereal.stars.purple.add(1).log10().sqrt().add(0.5)).min(100).toNumber();
+      this.wcount = Decimal.floor(player.endgame.ethereal.stars.white.add(1).log10().sqrt().add(0.5)).min(100).toNumber();
+      this.lcount = Decimal.floor(player.endgame.ethereal.stars.black.add(1).log10().sqrt().add(0.5)).min(100).toNumber();
+      this.acount = Decimal.floor(player.endgame.ethereal.stars.gray.add(1).log10().sqrt().add(0.5)).min(100).toNumber();
       this.isBlob = Theme.currentName() === "S11";
     },
     updateSize() {
@@ -68,56 +68,56 @@ export default {
     />
     <Star
       v-for="j in ocount"
-      :key="j"
+      :key="j + 100"
       :bounds="bounds"
       :is-blob="isBlob"
       :type="1"
     />
     <Star
       v-for="k in ycount"
-      :key="k"
+      :key="k + 200"
       :bounds="bounds"
       :is-blob="isBlob"
       :type="2"
     />
     <Star
       v-for="l in gcount"
-      :key="l"
+      :key="l + 300"
       :bounds="bounds"
       :is-blob="isBlob"
       :type="3"
     />
     <Star
       v-for="m in bcount"
-      :key="m"
+      :key="m + 400"
       :bounds="bounds"
       :is-blob="isBlob"
       :type="4"
     />
     <Star
       v-for="n in pcount"
-      :key="n"
+      :key="n + 500"
       :bounds="bounds"
       :is-blob="isBlob"
       :type="5"
     />
     <Star
       v-for="o in wcount"
-      :key="o"
+      :key="o + 600"
       :bounds="bounds"
       :is-blob="isBlob"
       :type="6"
     />
     <Star
       v-for="p in lcount"
-      :key="p"
+      :key="p + 700"
       :bounds="bounds"
       :is-blob="isBlob"
       :type="7"
     />
     <Star
       v-for="q in acount"
-      :key="q"
+      :key="q + 800"
       :bounds="bounds"
       :is-blob="isBlob"
       :type="8"
